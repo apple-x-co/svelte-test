b70adc337c68c1d9f6763b81d4535a8a97da3451
apple-x-co <apple.x.co@gmail.com>
2023-06-26 16:45:51 +0900

    Detail page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
 <h2>Home page</h2>
 
-<a href="blog/one">One</a>
-<a href="blog/two">Two</a>
-<a href="blog/three">Three</a>
+<a href="blog">Blog</a>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+    export let data;
+</script>
+
 <h2>Blog Detail</h2>
+<h3>{data.post.title}</h3>
+<div>{@html data.post.content}</div>
